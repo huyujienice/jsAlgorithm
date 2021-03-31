870f2639a189fd113de80eb9e567ef9516ebdb79
clarkhu@yeahka.com <13828732391@163.com>
2021-03-31 22:00:10 +0800

    add binary tree sort logic

--- rebuildBinarytree.js
+++ rebuildBinarytree.js
@@ -1,7 +1,37 @@
-class TreeNode {
+//节点定义
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
 }
+
+let a = new Node("A");
+let b = new Node("B");
+let c = new Node("C");
+let d = new Node("D");
+let e = new Node("E");
+let f = new Node("F");
+let g = new Node("G");
+let h = new Node("H");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+d.right = h;
+c.left = f;
+c.right = g;
+
+//前序遍历
+function preorderTraversal(root) {
+  let point = root;
+  console.log(`${point.value}`);
+  if (point.left) {
+    preorderTraversal(root.left);
+  } else {
+   
+  }
+}
+
+preorderTraversal(a);
