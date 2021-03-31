cc7c94b4d4e8c75f51598111f095893110580c1d
huyujienice <huyujienice@gmail.com>
2021-03-31 23:23:24 +0800

    add binarytree logic

--- rebuildBinarytree.js
+++ rebuildBinarytree.js
@@ -23,14 +23,40 @@ d.right = h;
 c.left = f;
 c.right = g;
 
+let ptA = [];
 //前序遍历
-function preorderTraversal(root) {
+function preorderTraversal(root, type) {
   let point = root;
   console.log(`${point.value}`);
-  if (point.left) {
-    preorderTraversal(root.left);
+  console.log(ptA);
+  if (type) {
+    if (point.right) {
+      preorderTraversal(point.right);
+    } else {
+      if (!ptA.length) {
+        let lastPoint = ptA.pop();
+        preorderTraversal(lastPoint, 1);
+      } else {
+        return;
+      }
+    }
   } else {
-   
+    if (point.left) {
+      ptA.push(point);
+      preorderTraversal(point.left);
+    } else {
+      if (point.right) {
+        ptA.push(point);
+        preorderTraversal(point.right);
+      } else {
+        if (!ptA.length) {
+          let lastPoint = ptA.pop();
+          preorderTraversal(lastPoint, 1);
+        } else {
+          return;
+        }
+      }
+    }
   }
 }
 
