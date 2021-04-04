d51bb27e4463750c3935dbe6ee623c9705e20b00
huyujienice <huyujienice@gmail.com>
2021-04-04 12:31:49 +0800

    :art:

--- rebuildBinarytree.js
+++ rebuildBinarytree.js
@@ -7,21 +7,32 @@ class Node {
   }
 }
 
-let a = new Node("A");
-let b = new Node("B");
-let c = new Node("C");
-let d = new Node("D");
-let e = new Node("E");
-let f = new Node("F");
-let g = new Node("G");
-let h = new Node("H");
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-d.right = h;
-c.left = f;
-c.right = g;
+function rebuildTree(){
+  
+}
+
+
+
+
+
+
+
+
+// let a = new Node("A");
+// let b = new Node("B");
+// let c = new Node("C");
+// let d = new Node("D");
+// let e = new Node("E");
+// let f = new Node("F");
+// let g = new Node("G");
+// let h = new Node("H");
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// d.right = h;
+// c.left = f;
+// c.right = g;
 
 //二叉树前序遍历
 //利用栈结构将需要回溯的节点保存
@@ -44,13 +55,13 @@ c.right = g;
 // }
 //二叉树前序遍历
 //直接递归
-function preorderTraversal(node) {
-  if (node) {
-    console.log(`${node.value}`);
-    preorderTraversal(node.left);
-    preorderTraversal(node.right);
-  }
-}
+// function preorderTraversal(node) {
+//   if (node) {
+//     console.log(`${node.value}`);
+//     preorderTraversal(node.left);
+//     preorderTraversal(node.right);
+//   }
+// }
 // preorderTraversal(a);
 
 //中序遍历
@@ -80,35 +91,35 @@ function preorderTraversal(node) {
 // postorderTraveral(a);
 
 //层序遍历
-function levelTraveral(node, arr = []) {
-  if (arr.length) {
-    let l = arr.length;
-    while (l > 0) {
-      let n = arr.shift();
-      if (n.left) {
-        console.log(`${n.left.value}`);
-        arr.push(n.left);
-      }
-      if (n.right) {
-        console.log(`${n.right.value}`);
-        arr.push(n.right);
-      }
-      l--;
-    }
-    levelTraveral(null, arr);
-  } else {
-    if (node) {
-      console.log(`${node.value}`);
-      if (node.left) {
-        console.log(`${node.left.value}`);
-        arr.push(node.left);
-      }
-      if (node.right) {
-        console.log(`${node.right.value}`);
-        arr.push(node.right);
-      }
-      levelTraveral(null, arr);
-    }
-  }
-}
-levelTraveral(a);
+// function levelTraveral(node, arr = []) {
+//   if (arr.length) {
+//     let l = arr.length;
+//     while (l > 0) {
+//       let n = arr.shift();
+//       if (n.left) {
+//         console.log(`${n.left.value}`);
+//         arr.push(n.left);
+//       }
+//       if (n.right) {
+//         console.log(`${n.right.value}`);
+//         arr.push(n.right);
+//       }
+//       l--;
+//     }
+//     levelTraveral(null, arr);
+//   } else {
+//     if (node) {
+//       console.log(`${node.value}`);
+//       if (node.left) {
+//         console.log(`${node.left.value}`);
+//         arr.push(node.left);
+//       }
+//       if (node.right) {
+//         console.log(`${node.right.value}`);
+//         arr.push(node.right);
+//       }
+//       levelTraveral(null, arr);
+//     }
+//   }
+// }
+// levelTraveral(a);
