54644313abfe38d06058ad7a5b9220449e1b2ae1
clarkhu@yeahka.com <13828732391@163.com>
2021-04-15 22:31:20 +0800

    add merge sort logic

--- sortArray.js
+++ sortArray.js
@@ -10,8 +10,8 @@ function createArray(n) {
   return arr;
 }
 
+//二分查找元素
 function binarySearch(arr, nums) {
-  //二分查找
   //基本条件，有序数组
   console.log(arr);
   if (arr.length == 1) {
@@ -40,4 +40,36 @@ function goBS(nums) {
   binarySearch(arr, nums);
 }
 
-goBS(3);
+function goMS() {
+  let arr = createArray(10);
+  console.log(arr);
+  let arrs = mergeSort(arr);
+  console.log(arrs);
+}
+
+//归并排序
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+//归并排序核心代码
+function merge(leftArr, rightArr) {
+  let result = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      result.push(leftArr.shift());
+    } else {
+      result.push(rightArr.shift());
+    }
+  }
+  return result.concat(leftArr).concat(rightArr);
+}
+
+goMS();
+
+
