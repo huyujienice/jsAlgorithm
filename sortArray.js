e05bfbc00eaef231be99ca72056e123fcba1dad4
huyujienice <huyujienice@gmail.com>
2021-05-04 14:13:00 +0800

    :art: add selectionSort logic in sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -141,6 +141,24 @@ function bubbleSort(arr) {
   return arr;
 }
 
+//选择排序
+//与冒泡排序有少许相似，注意区分
+/* 选择排序(Selection-sort)是一种简单直观的排序算法。它的工作原理：
+首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+以此类推，直到所有元素均排序完毕。 */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let mid = arr[j];
+        arr[j] = arr[i];
+        arr[i] = mid;
+      }
+    }
+  }
+  return arr;
+}
+
 function goSortArray(fn) {
   let arr = createArray(10);
   console.log(arr);
@@ -148,4 +166,4 @@ function goSortArray(fn) {
   console.log(arrs);
 }
 
-goSortArray(bubbleSort);
+goSortArray(selectionSort);
