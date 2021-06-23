34fe7406f72736385dac9c2dbc7ec5875a92c2d2
clarkhu@yeahka.com <13828732391@163.com>
2021-06-23 19:59:04 +0800

    :art: add greedy

--- cutLine.js
+++ cutLine.js
@@ -8,8 +8,8 @@
 function max(n) {
   if (n < 3) return 1;
   if (n == 3) return 2;
-  let arr = [1,1,2,3];//计算方程，第i项表示长度为i的绳子的最大乘积
-  for (let i = 4; i < n; i++) {
+  let arr = [1, 1, 2, 3]; //计算方程，第i项表示长度为i的绳子的最大乘积
+  for (let i = 4; i < n + 1; i++) {
     let m = 0;
     for (let j = 1; j < i; j++) {
       let r = arr[j] * arr[i - j];
@@ -18,9 +18,46 @@ function max(n) {
       }
     }
     arr[i] = m;
-
   }
-  console.log(arr);
+  console.log(arr[arr.length - 1]);
 }
 
-max(10);
+//贪心算法
+function simpleMax(n) {
+  if (n < 3) return 1;
+  if (n == 3) return 2;
+  let n1 = (n - (n % 3)) / 3 - 1;
+  let n2 = n1;
+  let r1 = 3;
+  while (n2 > 1) {
+    r1 = 3 * r1;
+    n2--;
+  }
+  let l1 = n - n1 * 3;
+  let r2 = 0;
+  switch (l1) {
+    case 6:
+      r2 = 9;
+      break;
+    case 5:
+      r2 = 6;
+      break;
+    case 4:
+      r2 = 4;
+      break;
+    case 3:
+      r2 = 3;
+      break;
+    case 2:
+      r2 = 2;
+      break;
+    case 1:
+      r2 = 1;
+      break;
+  }
+  let result = r1 * r2;
+  console.log(result);
+}
+
+max(22);
+simpleMax(22);
