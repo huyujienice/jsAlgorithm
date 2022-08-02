cc565ab56328317dc8501620d970ebd71eacbda6
clarkhu <huyujienice@gmail.com>
2022-08-02 16:48:11 +0800

    :sparkles:

--- tree/index.js
+++ tree/index.js
@@ -1,14 +1,14 @@
-//二叉树用二叉链表表示存储
+// 二叉树用二叉链表表示存储
 function Node(data, left, right) {
   this.data = data;
   this.left = left;
   this.right = right;
 }
 
-//二叉树用数组表示
-//把根结点存储在下标为1的位置，把左子结点存储在下标为2*i的位置，把右子结点存储在2*i+1的位置，以此类推  
-//所以，求一个结点的父结点：i/2即可
-//https://www.jianshu.com/p/a04d73417f7c
+// 二叉树用数组表示
+// 把根结点存储在下标为1的位置，把左子结点存储在下标为2*i的位置，把右子结点存储在2*i+1的位置，以此类推  
+// 所以，求一个结点的父结点：i/2即可
+// https://www.jianshu.com/p/a04d73417f7c
 
 // 二叉树前序遍历 leetcode 144  done    
 // N叉树前序遍历 leetcode 589  done  
