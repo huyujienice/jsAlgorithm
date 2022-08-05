ba9d57bf8906b7fca93b3b79e7486beddc9a359b
clarkhu <huyujienice@gmail.com>
2022-08-05 18:10:54 +0800

    :sparkles:

--- tree/index.js
+++ tree/index.js
@@ -10,9 +10,21 @@ function Node(data, left, right) {
 // 所以，求一个结点的父结点：i/2即可
 // https://www.jianshu.com/p/a04d73417f7c
 
-// 二叉树前序遍历 leetcode 144  done    
-// N叉树前序遍历 leetcode 589  done  
+// 二叉树前序遍历 leetcode 144  done  check  
+// N叉树前序遍历 leetcode 589  done  check  
 // 翻转二叉树 leetcode 226  done   
 // 层序遍历二叉树 leetcode 剑指offer 32  done    
 // 二叉树的层序遍历 leetcode 107  done  
+// 二叉树锯齿形层序遍历 leetcode 103 done  
+
+// 进阶
+// leetcode 110  done  
+// leetcode 112  done  
+// leetcode 105  done  
+// leetcode 222  ？  
+// leetcode 剑指offer 54  done
+// leetcode 剑指offer 26  done  
+// leetcode 662  
+// leetcode 968  
+
 
